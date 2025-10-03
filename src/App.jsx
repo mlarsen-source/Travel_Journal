7cc25b50af414d04ed06c1e7bf56fcaa06da508d
mlarsen <174657206+mlarsen-source@users.noreply.github.com>
2025-10-03 14:29:44 -0700

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import Header from "./components/Header"
 import Entry from "./components/Entry"
+import data from "./data.js"
 
 export default function App() {
+
+  const entryData = data.map((entry)=> {
+    return (
+      <Entry 
+        img={entry.img}
+        title={entry.title}
+        country={entry.country}
+        gLink={entry.googleMapsLink}
+        dates={entry.dates}
+        text={entry.text}
+      />
+    )
+  })
+
   return (
     <>
       <Header />
-      <Entry />
-      <Entry />
+      <main className="container">
+        {entryData}
+      </main>
     </>
   )
 }
